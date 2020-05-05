4f16f09d54d34b532fb1c2ee9e8aa7eaa109ea95
Claudia Maciel <clc_80@yahoo.com>
2020-05-04 19:45:59 -0700

    Initial Commit

---
+++ Virtual Tour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tour
+//
+//  Created by Claudia Contreras on 5/4/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
